0d6a00bc696757467754baa3a6da9e138b941143
Boris <abirme@yandex.ru>
2020-04-08 17:34:18 +0300

    Java Rush: task0436

---
+++ src/task/task04/task0436/Solution.java
@@ -0,0 +1,37 @@
+package task.task04.task0436;
+/*Ввести с клавиатуры два числа m и n.
+Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
+
+Пример: m=2, n=4
+8888
+8888
+
+Требования:
+•	Программа должна считывать два числа c клавиатуры.
+•	Программа должна выводить текст на экран.
+•	Программа должна выводить прямоугольник размером m на n из восьмёрок.
+•	В программе должен использоваться цикл for.*/
+/*
+Рисуем прямоугольник
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(8);
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
