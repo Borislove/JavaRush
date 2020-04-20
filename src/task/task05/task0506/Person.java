37e5ce92aefc52101dd097c587ce2afbc17dfe0c
Boris <abirme@yandex.ru>
2020-04-20 20:41:47 +0300

    Java Rush: task0506

---
+++ src/task/task05/task0506/Person.java
@@ -0,0 +1,28 @@
+package task.task05.task0506;
+
+/*Создать class Person. У человека должно быть имя String name,
+ возраст int age, адрес String address, пол char sex.
+
+Требования:
+•	Программа не должна считывать данные с клавиатуры.
+•	У класса Person должна быть переменная name с типом String.
+•	У класса Person должна быть переменная age с типом int.
+•	У класса Person должна быть переменная address с типом String.
+•	У класса Person должна быть переменная sex с типом char.
+•	Всего в классе должно быть 4 переменных.*/
+
+/*
+Человечки
+*/
+
+public class Person {
+    //напишите тут ваш код
+    String name;
+    int age;
+    String address;
+    char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
