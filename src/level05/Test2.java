799898ca2ec171f6cdbb601610926c31394fed47
Boris <abirme@yandex.ru>
2020-04-20 18:33:14 +0300

    Java Rush: task0501

---
+++ src/level05/Test2.java
@@ -0,0 +1,7 @@
+package level05;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Test1.main(args);
+    }
+}
---
+++ src/task/task05/task0501/Cat.java
@@ -0,0 +1,28 @@
+package task.task05.task0501;
+
+/*Создать класс Cat. У кота должно быть имя (name, String),
+возраст (age, int), вес (weight, int), сила (strength, int).
+
+Требования:
+•	Программа не должна считывать данные с клавиатуры.
+•	У класса Cat должна быть переменная name с типом String.
+•	У класса Cat должна быть переменная age с типом int.
+•	У класса Cat должна быть переменная weight с типом int.
+•	У класса Cat должна быть переменная strength с типом int.
+•	Всего в классе должно быть 4 переменных.*/
+
+/*
+Создание кота
+*/
+
+public class Cat {
+    //напишите тут ваш код
+
+    String name;
+    int age;
+    int weight;
+    int strengh;
+
+    public static void main(String[] args) {
+    }
+}
