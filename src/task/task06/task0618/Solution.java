ff4b9f2f9fd20b674613680f95341ea7bb8451e0
Borislove <abirme@yandex.ru>
2020-06-17 20:10:14 +0300

    Java Rush: task0618

---
+++ src/task/task06/task0618/Solution.java
@@ -0,0 +1,26 @@
+package task.task06.task0618;
+
+/*Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
+
+Требования:
+•	Создай класс KissMyShinyMetalAss внутри класса Solution.
+•	Класс KissMyShinyMetalAss должен быть статическим.
+•	Класс KissMyShinyMetalAss должен быть public.
+•	В методе main создай объект класса KissMyShinyMetalAss.
+•	В методе main выведи объект класса KissMyShinyMetalAss на экран.*/
+
+/*
+KissMyShinyMetalAss
+*/
+
+public class Solution {
+
+    public static class KissMyShinyMetalAss {
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        KissMyShinyMetalAss kissMyShinyMetalAss = new KissMyShinyMetalAss();
+        System.out.println(kissMyShinyMetalAss);
+    }
+}
