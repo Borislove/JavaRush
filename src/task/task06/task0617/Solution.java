99ee3dbad62c58bc3a7dba247444ee3cb8929475
Borislove <abirme@yandex.ru>
2020-06-17 19:57:43 +0300

    Java Rush: task0617

---
+++ src/task/task06/task0617/Solution.java
@@ -0,0 +1,39 @@
+package task.task06.task0617;
+
+/*1. В классе Solution создать public static класс Idea.
+2. В классе Idea создать метод public String getDescription(), который будет возвращать любую строку.
+3. В классе Solution создай статический метод public void printIdea(Idea idea),
+который будет выводить на экран описание идеи - это то, что возвращает метод getDescription().
+
+Требования:
+•	В классе Solution создать public static класс Idea.
+•	В классе Idea создать метод public String getDescription().
+•	Метод getDescription должен возвращать любую строку.
+•	В классе Solution создайте метод public static void printIdea(Idea idea).
+•	Метод printIdea должен выводить описание идеи на экран.*/
+
+/*
+Блокнот для новых идей
+*/
+public class Solution {
+
+    //напишите тут ваш код
+    public static class Idea {
+        public String getDescription() {
+            String str = "text";
+            System.out.println(str);
+            return str;
+        }
+    }
+
+    public static void printIdea(Idea idea) {
+        System.out.println(idea.getDescription());
+    }
+
+    public static void main(String[] args) {
+        printIdea(new Idea());
+    }
+}
+
+
+
